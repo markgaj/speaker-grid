0d4d44e97f5cc43ef6b48c79e2f8bf7ace132d55
Mark Gajadhar <markgaj@gmail.com>
2017-04-16 12:32:54 -0600

    Completed Admin form for speaker CPT.
    - Uses 2 wysiwyg editors

--- admin/speaker-profile.php
+++ admin/speaker-profile.php
@@ -37,30 +37,88 @@ function spkgd_build_metaboxes( $post ){
 }
 add_action( 'add_meta_boxes_spkgd_speaker', 'spkgd_build_metaboxes');
 
-function spkgd_speaker_details_callback(){
+function spkgd_speaker_details_callback( $post ){
+
+	$current_name 				= get_post_meta( $post->ID, 'spkgd_speaker_name', true );
+	$current_speaker_title 		= get_post_meta( $post->ID, 'spkgd_speaker_title', true );
+	$current_institution 		= get_post_meta( $post->ID, 'spkgd_institution', true );
+	$current_biography 			= get_post_meta( $post->ID, 'spkgd_biography', true );
 	?>
-	<!-- <div class="inside"> -->
-		<table class="form-table">
-				<tr>
-					<th>Name:</th>
-					<td><input type="text" name="name"/></td>
-				</tr>
 
-				<tr>
-					<th>Title:</th>
-					<td><input type="text" name="title"></td>
-				</tr>
+	<table class="form-table">
+		<tr>
+			<th>Name:</th>
+			<td><input type="text" name="name" size="30" 
+			value=<?php echo('"' . $current_name . '"');?> ></td>
+		</tr>
 
-				<tr>
-					<th>Institution:</th>
-					<td><input type="text" name="institution"></td>
-				</tr>
-		</table>
-	<!-- </div> -->
+		<tr>
+			<th>Title:</th>
+			<td><input type="text" name="speaker-title" size="60"
+			value=<?php echo('"' . $current_speaker_title . '"');?> ></td>
+		</tr>
+
+		<tr>
+			<th>Institution:</th>
+			<td><input type="text" name="institution" size="110"
+			value=<?php echo('"' . $current_institution . '"');?> ></td>
+		</tr>
+
+		<tr>
+			<th>Biography:</th>
+			<td><?php wp_editor( $current_biography, 'biography', array( 'wpautop' => false ) ); ?></td>
+		</tr>
+	</table>
+
 	<?php
-
 }
 
-function spkgd_presentation_details_callback(){
+function spkgd_presentation_details_callback( $post ){
+	$current_abstract_title		= get_post_meta( $post->ID, 'spkgd_abstract_title', true );
+	$current_abstract 			= get_post_meta( $post->ID, 'spkgd_abstract', true );
+	?>
 
+	<table class="form-table">
+		<tr>
+			<th>Title:</th>
+			<td><input type="text" name="abstract-title" size="30"
+			value=<?php echo('"' . $current_abstract_title . '"');?> ></td>
+		</tr>
+
+		<tr>
+			<th>Abstract:</th>
+			<td><?php wp_editor( $current_abstract, 'abstract', array( 'wpautop' => false ) ); ?></td>
+		</tr>
+	</table>
+
+	<?php
 }
+
+function spkgd_save_speaker( $post_id ){
+	//don't overwrite data if triggered by autosave
+	if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) 
+	return;
+
+/*	if ( !wp_verify_nonce( $_POST['___box_content_nonce'], plugin_basename( __FILE__ ) ) )
+	return;*/
+
+	// Make sure the user has permission to edit this post
+	if ( ! current_user_can( 'edit_post', $post_id ) ){
+		return;
+	}
+
+	$name 				= $_POST['name'];
+	$speaker_title 		= $_POST['speaker-title'];
+	$institution 		= $_POST['institution'];
+	$biography 			= $_POST['biography'];
+	$abstract_title		= $_POST['abstract-title'];
+	$abstract 			= $_POST['abstract'];
+
+	update_post_meta( $post_id, 'spkgd_speaker_name', $name );
+	update_post_meta( $post_id, 'spkgd_speaker_title', $speaker_title );
+	update_post_meta( $post_id, 'spkgd_institution', $institution );
+	update_post_meta( $post_id, 'spkgd_biography', $biography );
+	update_post_meta( $post_id, 'spkgd_abstract_title', $abstract_title );
+	update_post_meta( $post_id, 'spkgd_abstract', $abstract );
+}
+add_action( 'save_post', 'spkgd_save_speaker' );
