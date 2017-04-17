d46707013905a52d1a65cdbf3d5ebcbc757b561f
Mark Gajadhar <markgaj@gmail.com>
2017-04-17 17:39:00 -0600

    Refactored Shortcode.php
    -Now should work with multiple speakers

--- includes/shortcode.php
+++ includes/shortcode.php
@@ -1,48 +1,48 @@
 <?php
 function spkgd_display(){
 	$speakers = get_posts( array('post_type' => 'spkgd_speaker', 'numberposts' => -1) );
-
-	foreach ($speakers as $speaker){
-		$feature_image = wp_get_attachment_image_src( get_post_thumbnail_id( $speaker->ID ), 'full' )[0];
-		$speaker_data = spkgd_strip_array( (get_post_meta( $speaker->ID )) );
 		
-		ob_start(); ?>
+	ob_start(); ?>
 
 		<div class="spkgd_container">
-			<div class="spkgd_box">
-				<img src=<?php echo($feature_image);?> ></img>
-				<h2><?php echo($speaker_data["spkgd_speaker_name"]);?></h2>
-				<p><?php echo($speaker_data["spkgd_abstract_title"]);?></p>
-				<div class="spkgd_clear"></div>
-				<div class="spkgd_link-bar">
-					<div class="spkgd_speaker-bio-link">
-						<p>BIOGRAPHY <img src=<?php echo(SPKGD_ABSURL . '/images/expand_arrow.png');?> ></img></p>
+			<?php
+			foreach ($speakers as $speaker){
+				$feature_image = wp_get_attachment_image_src( get_post_thumbnail_id( $speaker->ID ), 'full' )[0];
+				$speaker_data = spkgd_strip_array( (get_post_meta( $speaker->ID )) );
+				?>
+					<div class="spkgd_box">
+						<img src=<?php echo($feature_image);?> ></img>
+						<h2><?php echo($speaker_data["spkgd_speaker_name"]);?></h2>
+						<p><?php echo($speaker_data["spkgd_abstract_title"]);?></p>
+						<div class="spkgd_clear"></div>
+						<div class="spkgd_link-bar">
+							<div class="spkgd_speaker-bio-link">
+								<p>BIOGRAPHY <img src=<?php echo(SPKGD_ABSURL . '/images/expand_arrow.png');?> ></img></p>
+							</div>
+							<div class="spkgd_abstract-link">
+								<p>ABSTRACT <img src=<?php echo(SPKGD_ABSURL . '/images/expand_arrow.png');?> ></img></p>
+							</div>
+						</div>
+						<div class="spkgd_clear"></div>
+						<div class="spkgd_speaker-bio">
+							<h1> <?php echo($speaker_data["spkgd_speaker_title"]);?></h1>
+							<div class="spkgd_institution">
+								<p>
+									<?php echo($speaker_data["spkgd_institution"]);?>
+								</p>
+							</div>
+							<?php echo($speaker_data["spkgd_biography"]);?>
+						</div>
+						<div class="spkgd_abstract">
+							<h1><?php echo($speaker_data["spkgd_abstract_title"]);?></h1>
+							<p>
+								<?php echo($speaker_data["spkgd_abstract"]);?>
+							</p>
+						</div>
 					</div>
-					<div class="spkgd_abstract-link">
-						<p>ABSTRACT <img src=<?php echo(SPKGD_ABSURL . '/images/expand_arrow.png');?> ></img></p>
-					</div>
-				</div>
-				<div class="spkgd_clear"></div>
-				<div class="spkgd_speaker-bio">
-					<h1> <?php echo($speaker_data["spkgd_speaker_title"]);?></h1>
-					<div class="spkgd_institution">
-						<p>
-							<?php echo($speaker_data["spkgd_institution"]);?>
-						</p>
-					</div>
-					<?php echo($speaker_data["spkgd_biography"]);?>
-				</div>
-				<div class="spkgd_abstract">
-					<h1><?php echo($speaker_data["spkgd_abstract_title"]);?></h1>
-					<p>
-						<?php echo($speaker_data["spkgd_abstract"]);?>
-					</p>
-				</div>
-			</div>
-		</div>
-
-		<?php
-	}
+				<?php
+			} ?>
+		</div> <?php
 	return ob_get_clean();
 }
 add_shortcode( 'spkgd', 'spkgd_display' );
